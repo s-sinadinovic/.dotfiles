19e86f8ffbd1773ef1cb0ea73e024a7d90adb283
s-sinadinovic <s.sinadinovic@icloud.com>
2023-04-02 22:14:30 +1000

    Add javascript to treesitter default lanuages

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -295,7 +295,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'javascript', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -417,8 +417,9 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+  rust_analyzer = {},
+  tsserver = {},
+  eslint = {},
 
   lua_ls = {
     Lua = {
