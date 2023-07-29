fd77ee682dbd3218d4f30c4aee29f240c60025e2
s-sinadinovic <s.sinadinovic@icloud.com>
2023-07-29 11:05:29 +1000

    Use temporary nvim config for fresh install

--- nvim/.config/nvim/__init.lua
+++ nvim/.config/nvim/__init.lua
@@ -142,9 +142,9 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
+    -- dependencies = {
+    --   'nvim-treesitter/nvim-treesitter-textobjects',
+    -- },
     config = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
