52f618e2b4c2210c013aff98ecc1d04f3abb14f6
s-sinadinovic <s.sinadinovic@icloud.com>
2024-05-09 07:41:05 +1000

    Remap < and > to keep selection when in v mode

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -280,6 +280,8 @@ vim.o.termguicolors = true
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ 'v' }, '<', '<gv', { silent = true })
+vim.keymap.set({ 'v' }, '<', '<gv', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -341,7 +343,7 @@ vim.keymap.set('n', '<leader>fb', ":Telescope file_browser<CR>", { desc = '[F]il
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'clangd' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -475,7 +477,7 @@ require('which-key').register({
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
+  clangd = {},
   gopls = {},
   pyright = {},
   rust_analyzer = {},
